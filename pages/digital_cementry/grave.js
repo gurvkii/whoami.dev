5ec4de0c1b74594e9f35b44d991eb1d2c0d314b7
Gruvkii <dev.gruvkii@gmail.com>
2026-09-25 09:14:44 +0545

    feat : Form Submission Done ( Need to save into local Storage )

--- pages/digital_cementry/grave.js
+++ pages/digital_cementry/grave.js
@@ -1,14 +1,42 @@
 const addBtn = document.getElementById("addBtn");
-const addform= document.getElementById("addform");
+const addform = document.getElementById("addform");
 const closeBtn = document.getElementById("closeBtn");
 
 
-addBtn.addEventListener("click", () =>{
+addBtn.addEventListener("click", () => {
     addform.showModal();
 })
 
-closeBtn.addEventListener("click", () =>
-{
+closeBtn.addEventListener("click", () => {
     addform.close();
 })
+
+
+
+const graveEpitaph = document.querySelector("#graveEpitaph");
+const cemetery = [];
+
+const graveform = document.querySelector("#graveForm");
+graveform.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const graveName = document.querySelector("#graveName").value;
+    const graveDesc = document.querySelector("#graveDescription").value;
+    const graveDeathDate = document.querySelector("#graveDeathDate").value;
+
+    const grave  = {
+        uid : crypto.randomUUID(),
+        graveName : graveName,
+        graveDesc:graveDesc,
+        graveDeathDate:graveDeathDate,
+    };
+     grave.push(cemetery);
+});
+
+
+
+
+
+
+
+
 
